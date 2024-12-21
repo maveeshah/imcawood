4ab51c5ad6f85eaf3f620456ec25d664fa8c7503
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-12-21 07:41:33 +0500

    feat: correct naming and fail checks in items

--- imcawood/extends/item.py
+++ imcawood/extends/item.py
@@ -1,9 +1,12 @@
 import frappe
 from erpnext.stock.doctype.item.item import Item
 
-class X_Item(Item):
+
+class CustomItem(Item):
     def validate(self):
-        super(X_Item, self).validate()
-        
-        self.custom_volume = self.custom_height*self.custom_width*self.custom_length
-    
+        super(CustomItem, self).validate()
+
+        if self.item_group and self.item_group == "Panels":
+            self.custom_volume = (
+                self.custom_height * self.custom_width * self.custom_length
+            )
